87640784d75b0a945578cf3127e676adf64d777f
Vladimir <vladimirelizarov89@gmail.com>
2018-07-09 12:32:35 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+    // new CleanWebpackPlugin(['dist']),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  mode: "development"
+};

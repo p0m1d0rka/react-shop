bb0699391291bd044c4715079efec33bf4fe64dc
Vladimir <vladimirelizarov89@gmail.com>
2018-07-09 13:55:15 +0300

    add hello world component

---
+++ src/components/hello_world/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  constructor(){
+    super()
+  }
+  render() {
+    return (
+      <div>hello world</div>
+    );
+  }
+}
+export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/hello_world'
 import './style.css';
 import Calculator from './calculator.js';
+
 
 console.log('hssdsdi')
 const c = new Calculator({firstNumber: 2, secondNumber: 4, operator: '-'})
 console.log(c.calc())
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('reactApp')
+);

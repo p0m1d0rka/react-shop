8bba8c6858784d0ab9e22a220fdc9a5f6852ce83
Vladimir <vladimirelizarov89@gmail.com>
2018-07-15 10:43:21 +0300

    add constants/products.js

---
+++ src/constants/products.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: 'Картофель',
+    price: 32.2,
+    imageUrl: '',
+  },
+  {
+    id: 2,
+    title: 'Томаты',
+    price: 123,
+    imageUrl: '',
+  },
+  {
+    id: 3,
+    title: 'Кабачки',
+    price: 21,
+    imageUrl: '',
+  },
+  {
+    id: 4,
+    title: 'Дыня',
+    price: 69.9,
+    imageUrl: '',
+  },
+]

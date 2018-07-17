f4f8ddbc98f9e5a127a5b663e135784f75876b49
Vladimir <vladimirelizarov89@gmail.com>
2018-07-17 09:23:55 +0300

    delete hello_world component

--- src/components/catalog_page.js
+++ src/components/catalog_page.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import HelloWorld from './hello_world/hello_world.js'
 import '../style.css';
-import Voucher from './voucher/voucher.js';
 import Catalog from './catalog/catalog.js';
 
 export default class CatalogPage extends React.Component{
@@ -9,8 +7,6 @@ export default class CatalogPage extends React.Component{
     const { products } = this.props
     return (
     <div>
-      <HelloWorld />
-      <Voucher items={[{item_id: 1, quantity: 1}, {item_id: 2, quantity: 5}]}/>
       <Catalog products={products}/>
     </div>
     );

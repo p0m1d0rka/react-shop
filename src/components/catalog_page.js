3097fa16658461bb96f1d6cf342ff2b6468db78e
Vladimir <vladimirelizarov89@gmail.com>
2018-07-23 15:03:20 +0300

    add custom quatity

--- src/components/catalog_page.js
+++ src/components/catalog_page.js
@@ -12,7 +12,7 @@ export default class CatalogPage extends React.Component{
     this.toCart = bind(this.toCart, this)
   }
 
-  toCart(e, id) {
+  toCart(e, id, quantity, dropQuantity) {
     const { products } = this.props
     const { productsInCart } = this.state
     let elem = find(productsInCart, {id})
@@ -21,17 +21,18 @@ export default class CatalogPage extends React.Component{
       each(productsInCart, item => {
         let mutateItem = Object.assign({}, item)
         if(mutateItem.id == id){
-          mutateItem.quantity ++
+          mutateItem.quantity = mutateItem.quantity + parseInt(quantity, 10)
         }
         new_state.push(mutateItem)
       })
       this.setState({productsInCart: new_state})
     }else{
       let new_state =  productsInCart.slice();
-      let new_product = find(products, {id})
-      new_state.push(merge(new_product, {quantity: 1}))
+      let new_product = find(products, { id })
+      new_state.push(merge(new_product, { quantity: parseInt(quantity, 10) }))
       this.setState({productsInCart: new_state})
     }
+    dropQuantity()
   }
 
   render() {
--- src/components/product_card/product_card.js
+++ src/components/product_card/product_card.js
@@ -3,15 +3,34 @@ import Image from '../image/image.js';
 import TextBox from '../text_box/text_box.js';
 import Price from '../price/price.js';
 import { CartManager } from '../catalog_page.js'
-import { Button } from 'reactstrap';
+import { Button, Input } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import './product_card.scss';
 
 
 export default class ProductCard extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { quantity: 1 }
+  }
+
+  handleQuantity = (e) =>{
+    let value = parseInt(e.target.value)
+    isNaN(value) ? 
+      e.target.value = 1 : 
+      e.target.value = value
+    this.setState({quantity: value}) 
+  }
+
+  dropQuantity = (e) => {
+    this.setState({quantity: 1})
+  }
+
   render() {
     const { product } = this.props
+    const { quantity } = this.state
+
     return (
       <div className='productCard'>
         <Image 
@@ -26,13 +45,14 @@ export default class ProductCard extends React.Component {
         <Price currency='USD' locale="en-US">
           { product.price }
         </Price>
+        <Input placeholder='Кол-во' value={ quantity } onChange={ (e) => this.handleQuantity(e) }/>
         <CartManager.Consumer >
           {
             toCart => {
               return (
                 <Button
                   color='success'
-                  onClick={(e) => toCart(e, product.id)}
+                  onClick={(e) => toCart(e, product.id, this.state.quantity, this.dropQuantity)}
                 >
                 <span>В корзину</span>
                 <FontAwesomeIcon icon={ faCartPlus }  size="1x"/>
